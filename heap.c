68931aa67b7f885b7521507876ef07d1b11de07b
RamiroMagnater1 <>
2022-05-07 02:16:33 +0000

    new try

--- heap.c
+++ heap.c
@@ -17,7 +17,7 @@ typedef struct Heap{
 } Heap;
 
 void* heap_top(Heap* pq){
-  if(pq->heapArray[0].data == NULL) return NULL;
+  if(pq->size == NULL) return NULL;
   return pq->heapArray[0].data;
 }
 
