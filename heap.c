072993d03057e4596716dae5f29708d021a0b6bb
RamiroMagnater1 <>
2022-05-07 02:04:31 +0000

    new try

--- heap.c
+++ heap.c
@@ -31,7 +31,7 @@ void heap_pop(Heap* pq){
 
 Heap* createHeap(){
   Heap * h = (Heap*) malloc(sizeof(Heap));
-  heapElem * harray = (heapElem*) malloc(4 * sizeof(heapElem));
+  h->harray = (heapElem*) malloc(3 * sizeof(heapElem));
   
   h->size = 0;
   h->capac = 3;
